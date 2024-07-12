3b9d589ee3077533e48918489f1a8cab2326044e
Delonios <delcontato2@gmail.com>
2024-07-11 23:18:27 -0300

    Feat: Criação de variáveis de ambiente concluído.  DESAFIO FINALIZADO.

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,7 @@ import { getTripDetails } from "./routes/get-trip-details";
 import { getParticipant } from "./routes/get-participant";
 import { error } from "console";
 import { errorHandler } from "./error-handler";
+import { env } from "./env";
 
 const app = fastify()
 
@@ -44,7 +45,7 @@ app.register(getParticipant)
 
 
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server running!')
 })
 
